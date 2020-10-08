7f713a460028cd43c668e8e3d928670d446a3095
Alex1Rosen <50190699+Alex1Rosen@users.noreply.github.com>
2020-10-08 14:14:32 -0700

    Create cameraTest.py

---
+++ cameraTest.py
@@ -0,0 +1,18 @@
+from time import sleep
+from picamera import PiCamera
+import keyboard
+
+camera = PiCamera()
+camera.resolution = (1024,768)
+camera.startpreview()
+#camera warm up
+sleep(2)
+
+run = True
+print("press q")
+
+while run:
+    if keyboard.is_pressed('q'):
+        print("snip snap")
+        camera.capture('foo.jpg')
+        run = False
