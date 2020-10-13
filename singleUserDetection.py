b7ff94e7898c11513ea984ab0472db37788425bb
Alex1Rosen <50190699+Alex1Rosen@users.noreply.github.com>
2020-10-13 12:57:35 -0700

    Update singleUserDetection.py

--- singleUserDetection.py
+++ singleUserDetection.py
@@ -15,13 +15,14 @@ import time
 # Get a reference to the Raspberry Pi camera.
 # If this fails, make sure you have a camera connected to the RPi and that you
 # enabled your camera in raspi-config and rebooted first.
-
+#os.system("espeak 'Door Booting' --stdout |aplay")
+print("defining")
 # Pin defs
-doorSignalPin = 18;# Broadcom pin
+doorSignalPin = 14;# Broadcom pin
 
 #Pin setup
-GPIO.setMode(GPIO.BCM)
-GPIO.setup(doorSignalPin, GPIO.out)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(doorSignalPin, GPIO.OUT)
 #initialize low
 GPIO.output(doorSignalPin, GPIO.LOW)
 
@@ -29,46 +30,40 @@ GPIO.output(doorSignalPin, GPIO.LOW)
 camera = picamera.PiCamera()
 camera.resolution = (320, 240)
 output = np.empty((240, 320, 3), dtype=np.uint8)
-
+print("about to encode images")
 # Load a sample picture and learn how to recognize it.
 print("Loading known face image(s)")
 alex_image = face_recognition.load_image_file("alex.jpg")
 alex_face_encoding = face_recognition.face_encodings(alex_image)[0]
-
+print("done encoding")
 # Initialize some variables
 face_locations = []
 face_encodings = []
 
 while True:
-    print("Capturing image.")
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format="rgb")
+ print("Capturing image.")
 
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(output)
-    print("Found {} faces in image.".format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
+ # Grab a single frame of video from the RPi camera as a numpy array
+ camera.capture(output, format="rgb")
 
-    # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([alex_face_encoding], face_encoding)
-        name = "<Unknown Person>"
+ # Find all the faces and face encodings in the current frame of video
+ face_locations = face_recognition.face_locations(output)
+ print("Found {} faces in image.".format(len(face_locations)))
+ face_encodings = face_recognition.face_encodings(output, face_locations)
+ # Loop over each face found in the frame to see if it's someone we know.
+ for face_encoding in face_encodings:
+  # See if the face is a match for the known face(s)
+   match = face_recognition.compare_faces([alex_face_encoding], face_encoding)
+   name = "<Unknown Person>"
+   if match[0]:
+    name = "Alex"
 
-        if match[0]:
-            name = "Alex"
-            lastDetectionTime = time.time()
+ if len(face_encodings) == 0:
+  name = "none"
 
-
-
-        print("I see someone named {}!".format(name))
-
-    # If a known face was seen in the last 10 UNITS then open the door
-    timeToKeepSignalHigh = 10
-    if (time.time() - lastDetectionTime) < timeToKeepSignalHigh and (time.time() > timeToKeepSignalHigh):
-        print("Door Signal HIGH")
-        GPIO.output(doorSignalPin, GPIO.HIGH)
-    else:
-        GPIO.output(doorSignalPin, GPIO.LOW)
-        print("Door Signal LOW")
-
+ if name == "Alex":
+  GPIO.output(doorSignalPin, GPIO.HIGH)
+  #os.system("espeak 'Welcome Alex' --stdout |aplay")
+  print("Setting High ")
+ else:
+  GPIO.output(doorSignalPin, GPIO.LOW)
